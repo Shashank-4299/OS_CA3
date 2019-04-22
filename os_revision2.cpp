089bd6f18ae58741daf7cbfa94d5d78140fe37c0
Shashank-4299 <49730768+Shashank-4299@users.noreply.github.com>
2019-04-22 23:01:54 +0530

    Add files via upload

---
+++ os_revision2.cpp
@@ -0,0 +1,19 @@
+for(i=0;i<n-1;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(pr[i]>pr[j])
+			{
+				temp1=pr[i];
+				pr[i]=pr[j];
+				pr[j]=temp1;
+				temp1=pt[i];
+				pt[i]=pt[j];
+				pt[j]=temp1;
+				strcpy(temp,p[i]);
+				strcpy(p[i],p[j]);
+				strcpy(p[j],temp);
+			}
+		}
+	}
+
