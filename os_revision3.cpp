88a89d81c0c9547021cd9b55b62473ddbe1891eb
Shashank-4299 <49730768+Shashank-4299@users.noreply.github.com>
2019-04-22 23:06:12 +0530

    Add files via upload

---
+++ os_revision3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+    printf("\norder of billed students ==>>\n");
+	printf("\nG_name\t P_time\t No.Gifts\n");
+	for(i=n-1;i>=0;i--)
+	{
+	   printf(" %s\t %d\t %d\t\n" ,p[i],pt[i],pr[i]);
+	}
+	getch();
+}
